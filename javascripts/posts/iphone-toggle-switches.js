eb1cbf912305b4e1b523c1a838fbb8794819387a
Elijah Miller <elijah.miller@gmail.com>
2009-08-06 12:29:31 -0400

    One more IE8 fix!

--- javascripts/posts/iphone-toggle-switches.js
+++ javascripts/posts/iphone-toggle-switches.js
@@ -1,9 +1,9 @@
 document.observe("dom:loaded", function() {
-  new iPhoneStyle('#variable_size_male_female', { checkedLabel: 'Female', uncheckedLabel: 'Male' });
-  new iPhoneStyle('#variable_size_a_b', { checkedLabel: 'A', uncheckedLabel: 'B' });
-  new iPhoneStyle('#static_size_a', { checkedLabel: '1', uncheckedLabel: '0', resizeContainer: false, resizeHandle: false });
-  new iPhoneStyle('#static_size_b', { checkedLabel: 'On', uncheckedLabel: 'Off', resizeContainer: false, resizeHandle: false });
-  new iPhoneStyle('#static_size_c', { checkedLabel: 'Override', uncheckedLabel: 'Default', resizeContainer: false, resizeHandle: false });
-  new iPhoneStyle('#draggable', { checkedLabel: 'Maybe you should drag me', uncheckedLabel: 'Weeeeeeeee', resizeHandle: false });
-  new iPhoneStyle('#sliding_labels', { checkedLabel: '<<<<<<<<<<<<<<<<<<<<<', uncheckedLabel: '>>>>>>>>>>>>>>>>>>>>>', resizeHandle: false });
+  new iPhoneStyle('input#variable_size_male_female', { checkedLabel: 'Female', uncheckedLabel: 'Male' });
+  new iPhoneStyle('input#variable_size_a_b', { checkedLabel: 'A', uncheckedLabel: 'B' });
+  new iPhoneStyle('input#static_size_a', { checkedLabel: '1', uncheckedLabel: '0', resizeContainer: false, resizeHandle: false });
+  new iPhoneStyle('input#static_size_b', { checkedLabel: 'On', uncheckedLabel: 'Off', resizeContainer: false, resizeHandle: false });
+  new iPhoneStyle('input#static_size_c', { checkedLabel: 'Override', uncheckedLabel: 'Default', resizeContainer: false, resizeHandle: false });
+  new iPhoneStyle('input#draggable', { checkedLabel: 'Maybe you should drag me', uncheckedLabel: 'Weeeeeeeee', resizeHandle: false });
+  new iPhoneStyle('input#sliding_labels', { checkedLabel: '<<<<<<<<<<<<<<<<<<<<<', uncheckedLabel: '>>>>>>>>>>>>>>>>>>>>>', resizeHandle: false });
 });
